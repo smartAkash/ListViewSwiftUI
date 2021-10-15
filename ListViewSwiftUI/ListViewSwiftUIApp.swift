9501e37ebce7e7149b88f1e7dcb39ada9a2fed77
Akash <akash.nara@smartsensesolutions.com>
2021-10-15 15:51:43 +0530

    Initial Commit

---
+++ ListViewSwiftUI/ListViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSwiftUIApp.swift
+//  ListViewSwiftUI
+//
+//  Created by Akash on 15/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
